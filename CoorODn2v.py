5bc8d759325ffe2c17399eefbb35da119cd04a55
Logan Lin <juniormail@126.com>
2018-11-28 18:51:18 +0800

    Change graph to directed graph, add arrow drawing functionality

--- CoorODn2v.py
+++ CoorODn2v.py
@@ -3,6 +3,7 @@ import pandas as pd
 from node2vec import Node2Vec
 from sklearn.cluster import KMeans
 from tqdm import *
+import matplotlib.pyplot as plt
 
 
 class CoorODn2v:
@@ -25,9 +26,12 @@ class CoorODn2v:
         self.kmeans = None
         self.n2v_wv = None
         self.graph = None
-        self.od_set = None
     
     def fit(self, dataset, walk_length=10, num_walks=200, weight_key='weight', workers=47):
+        '''
+        :param dataset: Coordinate dataset. Must at least contains coordinate columns `['longitude', 'latitude']`,
+        time column `['timestamp']` and group index `['orderid']`.
+        '''
         def fetch_od(dataset: pd.DataFrame):
             order_series = dataset['orderid'].drop_duplicates()
 
@@ -56,23 +60,24 @@ class CoorODn2v:
                     add_od_to_graph(graph, group.iloc[0]['cluster'], group.iloc[1]['cluster'])
                     tqdm_iter.update(1)
         
-        self.graph = nx.Graph()
+        self.graph = nx.DiGraph()
         
         self.od_set = fetch_od(dataset)
         self.od_set.index = range(self.od_set.shape[0])
         
-        self.kmeans = KMeans(n_clusters=self.clusters, n_jobs=workers).fit(od_set[['longitude', 'latitude']])
         print('Fitting KMeans model...')
+        self.kmeans = KMeans(n_clusters=self.clusters, n_jobs=workers).fit(self.od_set[['longitude', 'latitude']])
         self.od_set['cluster'] = self.kmeans.labels_
         
         add_dataset_to_graph(self.od_set, self.graph)
         
-        n2v = Node2Vec(self.graph, dimensions=self.dimensions, walk_length=10, num_walks=200, workers=workers, weight_key='weight')
+        n2v = Node2Vec(self.graph, dimensions=self.dimensions, walk_length=10, num_walks=200, 
+                       workers=workers, weight_key='weight')
         print('Processing random walk...')
         model = n2v.fit(window=10, min_count=1, batch_words=4)
         
         self.n2v_wv = model.wv
-        print('Finished fitting all models!')
+        print('Finished fitting model!')
         
     def predict(self, test_set):
         if self.kmeans is None:
@@ -93,12 +98,39 @@ class CoorODn2v:
         
         return embeddings
     
-    def plot_clusters(self, figsize=(10, 10)):
+    def plot_clusters(self, figsize=(10, 10), dpi=100, draw_arrows=False):
         if self.od_set is None:
             raise ValueError('Model is not trained yet.')
         
-        plt.figure(figsize=figsize)
+        plt.figure(figsize=figsize, dpi=dpi)
+        ax = plt.gca()
+        
+        if draw_arrows:
+            self.draw_graph_arrows(ax, thre=200, norm=300, color='#f9f568')
+        
         plt.scatter(self.od_set['longitude'], self.od_set['latitude'], alpha=0.2, s=1, c='#3677af')
         centers = self.kmeans.cluster_centers_
-        plt.scatter(centers[:, 0], centers[:, 1], c='red')
+        plt.scatter(centers[:, 0], centers[:, 1], c='red', marker='D', s=40)
         plt.show()
+        
+    def draw_graph_arrows(self, ax, color='red', thre=50, norm=10):
+        coor = self.kmeans.cluster_centers_
+        
+        for edge in self.graph.edges:
+            start, end = edge[0:2]
+            if start == end:
+                continue
+            weight = self.graph[start][end]['weight']
+
+            if weight < thre:
+                continue
+
+            x1 = coor[start][0]
+            y1 = coor[start][1]
+            x2 = coor[end][0]
+            y2 = coor[end][1]
+            width = (weight-thre)/norm
+
+            ax.annotate('', xy = (x2, y2),xytext = (x1, y1), fontsize = 7, color=color, 
+                        arrowprops=dict(edgecolor='black', facecolor=color, shrinkA=0, 
+                                        shrinkB=0, width=width, headwidth=max(width*3, 4)))
